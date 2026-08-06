09a1570df1756ccebf2a58286c3648d0f17bf892
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-08-06 17:48:07 -0300

    feat: adicionado controller para status do server

---
+++ src/main/java/com/oxeschool/api/controllers/ServerController.java
@@ -0,0 +1,16 @@
+package com.oxeschool.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ServerController {
+
+    @GetMapping
+    public String test(){
+        return "ok";
+    }
+
+}
