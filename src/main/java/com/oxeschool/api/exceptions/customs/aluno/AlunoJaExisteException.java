43cae42008207d67b0d51f4f1dd52323b5934a86
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-08-06 21:30:46 -0300

    docs: atualiza a documentação chore: edita mensagem de AlunoJaExisteException

--- src/main/java/com/oxeschool/api/exceptions/customs/aluno/AlunoJaExisteException.java
+++ src/main/java/com/oxeschool/api/exceptions/customs/aluno/AlunoJaExisteException.java
@@ -2,6 +2,6 @@ package com.oxeschool.api.exceptions.customs.aluno;
 
 public class AlunoJaExisteException extends RuntimeException {
     public AlunoJaExisteException() {
-        super("O aluno com esse email já existe");
+        super("Já existe um aluno com esse email");
     }
 }
