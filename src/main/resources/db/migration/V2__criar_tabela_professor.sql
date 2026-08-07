0e31394225f934e90320f1fea0ae86df97b89433
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-08-06 21:28:15 -0300

    fix: remove virgula no final da ultima coluna fix: aumenta tamanho da coluna de senhas

--- src/main/resources/db/migration/V2__criar_tabela_professor.sql
+++ src/main/resources/db/migration/V2__criar_tabela_professor.sql
@@ -3,6 +3,6 @@ CREATE TABLE professores(
     id BIGSERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    senha VARCHAR(50) NOT NULL,
+    senha VARCHAR(255) NOT NULL
 
 );
