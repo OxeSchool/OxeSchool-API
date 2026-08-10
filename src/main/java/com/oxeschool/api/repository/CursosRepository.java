0c1bcbdeb560f2da3184dfb972719cf8d9d14f6d
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-08-10 12:06:08 -0300

    refactor: exclui metodos para simplicar complexidade

--- src/main/java/com/oxeschool/api/repository/CursosRepository.java
+++ src/main/java/com/oxeschool/api/repository/CursosRepository.java
@@ -1,43 +1,12 @@
 package com.oxeschool.api.repository;
 
-import com.oxeschool.api.entity.Curso.Aulas;
 import com.oxeschool.api.entity.Curso.CursoEntity;
-import com.oxeschool.api.entity.Curso.Modulos;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.mongodb.repository.Update;
 
 import java.util.UUID;
 
 public interface CursosRepository extends MongoRepository<CursoEntity, UUID> {
 
     Boolean existsByNomeAndIdProfessor(String nome, Long idProfessor);
-
-    @Update("{ '$push': { 'modulos': ?1 } }")
-    @Query("{ '_id': ?0 }")
-    void adicionarModulo(UUID idCurso, Modulos modulo);
-
-    @Update("{ '$push': { 'modulos.$.aulas': ?2 } }")
-    @Query("{ '_id': ?0, 'modulos.id': ?1 }")
-    void adicionarAula(
-            UUID idCurso,
-            UUID idModulo,
-            Aulas aula
-    );
-
-    @Update("{ '$pull': { 'modulos': { 'id': ?1 } } }")
-    @Query("{ '_id': ?0 }")
-    void removerModulo(
-            UUID idCurso,
-            UUID idModulo
-    );
-
-    @Update("{ '$pull': { 'modulos.$[modulo].aulas': { 'id': ?2 } } }")
-    @Query("{ '_id': ?0 }")
-    void removerAula(
-            UUID idCurso,
-            UUID idModulo,
-            UUID idAula
-    );
 
 }
