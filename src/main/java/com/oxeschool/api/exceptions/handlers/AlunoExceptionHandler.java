f85a43cff8713f4e8aceec57d1aa63f2aab4aab2
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-08-10 15:13:29 -0300

    feat: criar exception e handler para aluno não encontrado

--- src/main/java/com/oxeschool/api/exceptions/handlers/AlunoExceptionHandler.java
+++ src/main/java/com/oxeschool/api/exceptions/handlers/AlunoExceptionHandler.java
@@ -2,6 +2,7 @@ package com.oxeschool.api.exceptions.handlers;
 
 import com.oxeschool.api.exceptions.CustomErrorResponse;
 import com.oxeschool.api.exceptions.customs.aluno.AlunoJaExisteException;
+import com.oxeschool.api.exceptions.customs.aluno.AlunoNaoEncontradoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +14,11 @@ public class AlunoExceptionHandler {
     @ExceptionHandler(AlunoJaExisteException.class)
     public ResponseEntity<CustomErrorResponse> alunoJaExisteExceptionHandler(AlunoJaExisteException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new CustomErrorResponse(exception.getMessage(), 409));
+    }
+
+    @ExceptionHandler(AlunoNaoEncontradoException.class)
+    public ResponseEntity<CustomErrorResponse> alunoNaoEncontradoExceptionHandler(AlunoNaoEncontradoException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new CustomErrorResponse(exception.getMessage(), 404));
     }
 
 }
