6479c5861b7eb7c70b38b7c2cdb6eec8f7bc536b
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-08-11 20:08:36 -0300

    refactor: desabilita temporariamente o filtro

--- src/main/java/com/oxeschool/api/configurations/SecurityConfigurations.java
+++ src/main/java/com/oxeschool/api/configurations/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package com.oxeschool.api.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfigurations {
 
 
@@ -32,10 +34,13 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers(HttpMethod.POST,"/registrar/**").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/login/**").permitAll()
-                        .requestMatchers("/auth/refresh").permitAll()
-                        .anyRequest().authenticated()
+//                        .requestMatchers(HttpMethod.POST,"/registrar/**").permitAll()
+//                        .requestMatchers(HttpMethod.POST,"/login/**").permitAll()
+//                        .requestMatchers("/auth/refresh").permitAll()
+//                        .anyRequest().authenticated()
+
+                        //TODO retirar isso
+                                .anyRequest().permitAll()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
